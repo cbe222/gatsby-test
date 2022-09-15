4eeab0a61fd6daa0a166a171fb55d033d8063edf
IR <ir@Tests-MacBook-Pro.local>
2022-09-14 17:36:07 -0700

    tst

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { Button } from "antd";
 
 export default function Home() {
   return <div>
     <div>Hello world</div>
     <div>This is BunnyLOL time!</div>
+    <button type="primary">I'm a button</button>
   </div>;
 }
